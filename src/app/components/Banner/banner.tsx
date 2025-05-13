6c0d1af8d6dbbef1562b21b42eea461c1b93a947
Nihal Gawale <nihalgawale123@gmail.com>
2025-05-13 18:54:48 +0530

    added banner video for mobile view

--- src/app/components/Banner/banner.tsx
+++ src/app/components/Banner/banner.tsx
@@ -26,15 +26,14 @@ function Banner() {
   return (
     <div className="md:relative w-full h-[950px]  md:h-[900px]  flex flex-col md:flex-none md:justify-start md:items-start ">
       <div className="w-full h-[600] md:h-full relative">
-        <Image
-          src={
-            width > breakpoint
-              ? "/assets/bannerImage.png"
-              : "/assets/mobileBannerImg.png"
-          }
-          alt="sneaker2"
-          fill={true}
-        />
+        {width > breakpoint ? (
+          <Image src="/assets/bannerImage.png" alt="sneaker2" fill={true} />
+        ) : (
+          <video width="100%" height="auto" controls autoPlay muted loop>
+            <source src="/videos/bannerImageVideo.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        )}
       </div>
       <div className="md:absolute md:mt-52 w-full h-[350px] md:w-[70%] md:z-10 md:flex md:justify-center">
         <BannerDetails width={width} breakpoint={breakpoint} />
