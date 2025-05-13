90a5d970a740bc421646119f1a68c768940d5fcc
Nihal Gawale <nihalgawale123@gmail.com>
2025-05-13 13:09:19 +0530

    working on enhancements in navBar

--- src/app/Theme/Fonts/Fonts.ts
+++ src/app/Theme/Fonts/Fonts.ts
@@ -1,20 +1,8 @@
 // fonts.ts
-import { Anton, Bebas_Neue, Roboto, } from "next/font/google";
+import { Bebas_Neue, Roboto } from "next/font/google";
 
 export const roboto = Roboto({
   subsets: ["latin"],
   variable: "--font-roboto-mono",
   display: "swap",
-});
-
-export const bebasNeue = Bebas_Neue({
-  subsets: ["latin"],
-  weight: "400",
-  variable: "--font-anton",
-});
-
-export const anton = Anton({
-  subsets: ["latin"],
-  weight: "400",
-  variable: "--font-anton",
 });
--- src/app/components/NavBar/navBar.tsx
+++ src/app/components/NavBar/navBar.tsx
@@ -1,28 +1,37 @@
 import Button from "@/app/Theme/UI/Button";
+import Image from "next/image";
 import React from "react";
 
 const navBaritems = [
   { key: "home", value: "Home" },
-  { key: "account", value: "Account" },
+  { key: "about", value: "About" },
   { key: "newDrops", value: "New Drops" },
 ];
 function NavBar() {
   return (
-    <div className="w-full h-[72px] flex items-center px-5 md:px-16 md:justify-around justify-between">
-      <div>LOGO</div>
+    <div className="w-full h-[72px] flex relative items-center  md:px-16 md:justify-around ">
+      <div className="w-[100px] h-[65px] relative mt-[9px]">
+        <Image src="/assets/BrandLogo.jpeg" fill={true} alt="brand-logo" />
+      </div>
+      <div className="w-[150px] h-[70px] relative mt-[10px]">
+      <Image src="/assets/brandNameImage.png" fill={true} alt="brand-name-logo" />
+
+      </div>
       <div className=" items-center space-x-20 hidden md:flex ">
         {navBaritems?.map((item) => (
           <div key={item?.key}>{item?.value}</div>
         ))}
       </div>
-      <div className="flex items-center">
+
+      {/* Temporarily commented the button */}
+      {/* <div className="flex items-center">
         <button
           type="button"
           className="bg-white text-black md:px-10 md:py-[15px] font-semibold text-sm px-6 py-2 font-roboto flex justify-center items-center"
         >
           SHOP
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
